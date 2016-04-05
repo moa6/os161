4e770343c4c2034f229272ed8d1321221ae04db2
augustinew <augustinew@ece.ubc.ca>
2016-04-04 22:45:23 -0700

    Initial implementation of swap

--- kern/vm/coremap.c
+++ kern/vm/coremap.c
@@ -33,7 +33,11 @@
 #include <spinlock.h>
 #include <synch.h>
 #include <vm.h>
+#include <proc.h>
+#include <addrspace.h>
 #include <kern/errno.h>
+#include <bitmap.h>
+#include <swap.h>
 #include <coremap.h>
 
 struct coremap *coremap;
@@ -42,37 +46,41 @@ void
 coremap_entry_init(struct coremap_entry *coremap_entry) {
 	KASSERT(coremap_entry != NULL);
 
-	coremap_entry->vaddr = 0;
-	coremap_entry->next = 0;
-	coremap_entry->allocated = false;
+	coremap_entry->ce_addrspace = NULL;
+	coremap_entry->ce_allocated = false;
+	coremap_entry->ce_foruser = false;
+	coremap_entry->ce_busy = false;
+	coremap_entry->ce_next = 0;
+	coremap_entry->ce_pgentry = NULL;
+	coremap_entry->ce_swapoffset = -1;
 }
 
 void
 coremap_bootstrap(void) {
 	paddr_t firstpaddr;
 	paddr_t lastpaddr;
-	unsigned long total_ppages;
+	unsigned long total_npages;
 
 	lastpaddr = ram_getsize();
 	KASSERT((lastpaddr & PAGE_FRAME) == lastpaddr);
-	total_ppages = (unsigned long)(lastpaddr / PAGE_SIZE);
+	total_npages = (unsigned long)(lastpaddr / PAGE_SIZE);
 
 	coremap = kmalloc(sizeof(struct coremap));
 	if (coremap == NULL) {
 		panic("kmalloc failed in coremap_boostrap\n");
 	}
 
-	coremap->ready = false;
+	coremap->c_ready = false;
 
-	spinlock_init(&coremap->c_lock);
+	spinlock_init(&coremap->c_spinlock);
 
-	coremap->c_entries = (struct coremap_entry*)kmalloc(total_ppages * sizeof(struct
+	coremap->c_entries = (struct coremap_entry*)kmalloc(total_npages * sizeof(struct
 	coremap_entry));
 	if (coremap->c_entries == NULL) {
 		panic("kmalloc failed in coremap_bootstrap\n");
 	}
 
-	for (unsigned long i=0; i<total_ppages; i++) {
+	for (unsigned long i=0; i<total_npages; i++) {
 		coremap_entry_init(&coremap->c_entries[i]);
 	}
 
@@ -80,12 +88,11 @@ coremap_bootstrap(void) {
 	KASSERT((firstpaddr & PAGE_FRAME) == firstpaddr);
 
 	for (unsigned long i=0; i<(unsigned long)(firstpaddr/PAGE_SIZE); i++) {
-		coremap->c_entries[i].allocated = true;
+		coremap->c_entries[i].ce_allocated = true;
 	}
 
-	coremap->total_ppages = total_ppages;
-	coremap->firstpaddr = firstpaddr;
-	coremap->ready = true;
+	coremap->c_npages = total_npages;
+	coremap->c_ready = true;
 }
 
 bool
@@ -93,32 +100,47 @@ coremap_ready(void) {
 	if (coremap == NULL) {
 		return false;
 	} else {
-		return coremap->ready;
+		return coremap->c_ready;
 	}
 }
 
 paddr_t
-coremap_getppages(unsigned long npages) {	
+coremap_getkpages(unsigned long npages) {	
 
-	paddr_t pframe;
-	unsigned long total_ppages;
+	paddr_t paddr;
+	paddr_t pgvictim;
+	int index;
+	int result;
+	unsigned long total_npages;
 	unsigned long start;
 	unsigned long end;
 
-	spinlock_acquire(&coremap->c_lock);
-	start = (unsigned long)(coremap->firstpaddr/PAGE_SIZE);
-	total_ppages = coremap->total_ppages;
+	spinlock_acquire(&coremap->c_spinlock);
+	start = 0;
+	total_npages = coremap->c_npages;
 	
-	while (start < total_ppages) {
-		if (!coremap->c_entries[start].allocated) {
+	while (start < total_npages) {
+		if (!coremap->c_entries[start].ce_allocated) {
 			end = start+npages-1;
-			if (end >= total_ppages) {
+			if (end >= total_npages) {
 				break;
 			}
 
 			while (end > start) {
-				if (coremap->c_entries[end].allocated) {
+
+				if (!coremap->c_entries[end].ce_allocated) {
+					KASSERT(coremap->c_entries[end].ce_addrspace
+					== NULL);
+					KASSERT(!coremap->c_entries[end].ce_busy);
+					KASSERT(coremap->c_entries[end].ce_next
+					== 0);
+					KASSERT(coremap->c_entries[end].ce_pgentry == NULL);
+					KASSERT(coremap->c_entries[end].ce_swapoffset == -1);
+				}
+
+				if (coremap->c_entries[end].ce_allocated) {
 					break;
+
 				} else {
 					end--;
 				}
@@ -126,18 +148,20 @@ coremap_getppages(unsigned long npages) {
 		
 			if (start == end) {
 				for (unsigned i=start; i<start+npages; i++) {
-					coremap->c_entries[i].allocated = true;
+					coremap->c_entries[i].ce_allocated = true;
+					coremap->c_entries[i].ce_foruser =
+					false;
 
 					if (i < start+npages-1) {
-						coremap->c_entries[i].next = 1;
+						coremap->c_entries[i].ce_next = 1;
 					} else {
-						coremap->c_entries[i].next = 0;
-					}				
+						coremap->c_entries[i].ce_next = 0;
+					}		
 				}
 
-				pframe = (paddr_t)(start*PAGE_SIZE);
-				spinlock_release(&coremap->c_lock);
-				return pframe;
+				paddr = (paddr_t)(start*PAGE_SIZE);
+				spinlock_release(&coremap->c_spinlock);
+				return paddr;
 			} 
 					
 		}
@@ -145,25 +169,279 @@ coremap_getppages(unsigned long npages) {
 		start++;
 	}
 
-	spinlock_release(&coremap->c_lock);
+	if (npages == 1) {
+
+		sw_getpage(&paddr);
+		index = (int)(paddr/PAGE_SIZE);
+		coremap->c_entries[index].ce_busy = true;
+
+		result = sw_evictpage(paddr);
+		if (result) {
+			coremap->c_entries[index].ce_busy = false;
+			return result;
+		}
+		
+		coremap->c_entries[index].ce_addrspace = NULL;
+		coremap->c_entries[index].ce_allocated = true;
+		coremap->c_entries[index].ce_foruser = false;
+		coremap->c_entries[index].ce_next = 0;
+		coremap->c_entries[index].ce_pgentry = NULL;
+		coremap->c_entries[index].ce_swapoffset = -1;			
+		coremap->c_entries[index].ce_busy = false;
+
+		spinlock_release(&coremap->c_spinlock);
+		return paddr;
+	
+	} else {
+		start = 0;
+		total_npages = coremap->c_npages;
+		
+		while (start < total_npages) {
+
+			if (!coremap->c_entries[end].ce_allocated) {
+				KASSERT(coremap->c_entries[end].ce_addrspace
+				== NULL);
+				KASSERT(!coremap->c_entries[end].ce_busy);
+				KASSERT(coremap->c_entries[end].ce_next
+				== 0);
+				KASSERT(coremap->c_entries[end].ce_pgentry == NULL);
+				KASSERT(coremap->c_entries[end].ce_swapoffset == -1);
+			}
+
+			if (!coremap->c_entries[start].ce_allocated ||
+			(!coremap->c_entries[start].ce_busy &&
+			coremap->c_entries[start].ce_foruser)) {
+				end = start+npages-1;
+				if (end >= total_npages) {
+					break;
+				}
+
+				while (end > start) {
+
+					if (coremap->c_entries[end].ce_foruser) {
+						KASSERT(coremap->c_entries[end].ce_pgentry
+						!= NULL);
+					}
+
+					if ((coremap->c_entries[end].ce_allocated &&
+					!coremap->c_entries[end].ce_foruser) ||
+					(coremap->c_entries[end].ce_foruser &&
+					coremap->c_entries[end].ce_busy)) {
+						break;
+					} else {
+						end--;
+					}
+				}
+			
+				if (start == end) {
+					for (unsigned i=start; i<start+npages; i++) {
+						
+						if (coremap->c_entries[i].ce_allocated) {
+							KASSERT(coremap->c_entries[i].ce_foruser);
+
+							coremap->c_entries[i].ce_busy
+							= true;
+							pgvictim =
+							(paddr_t)(i*PAGE_SIZE);
+
+							KASSERT((*coremap->c_entries[i].ce_pgentry
+							& PG_FRAME) << 12 ==
+							(int)pgvictim);
+
+							result =
+							sw_evictpage(pgvictim);
+							if (result) {
+								coremap->c_entries[i].ce_busy
+								= false;
+								spinlock_release(&coremap->c_spinlock);
+								return 0;
+							}
+
+							coremap->c_entries[i].ce_addrspace
+							= NULL;
+							coremap->c_entries[i].ce_allocated
+							= false;
+							coremap->c_entries[i].ce_foruser
+							= false;
+							coremap->c_entries[i].ce_next
+							= 0;
+							coremap->c_entries[i].ce_pgentry
+							= NULL;
+							coremap->c_entries[i].ce_swapoffset
+							= -1;
+							coremap->c_entries[i].ce_busy
+							= false;
+						} 
+						
+					}
+					
+					for (unsigned i=start; i<start+npages; i++) { 
+
+						coremap->c_entries[i].ce_addrspace
+						= NULL;
+						coremap->c_entries[i].ce_allocated
+						= true;
+						coremap->c_entries[i].ce_foruser
+						= false;
+						coremap->c_entries[i].ce_busy
+						= false;
+						coremap->c_entries[i].ce_pgentry
+						= NULL;
+						coremap->c_entries[i].ce_swapoffset
+						= -1;
+						
+
+						if (i < start+npages-1) {
+							coremap->c_entries[i].ce_next = 1;
+						} else {
+							coremap->c_entries[i].ce_next = 0;
+						}
+						
+					}
+
+					paddr = (paddr_t)(start*PAGE_SIZE);
+					spinlock_release(&coremap->c_spinlock);
+					return paddr;
+				} 
+						
+			}
+
+			start++;
+		}
+
+
+	}
+ 
+	spinlock_release(&coremap->c_spinlock);
 	return 0;
 }
 
-void
-coremap_freeppages(paddr_t pframe) {
-	unsigned long index;
+int
+coremap_getpage(int *pg_entry) {	
+	KASSERT(pg_entry != NULL);
 
-	spinlock_acquire(&coremap->c_lock);
-	index = (unsigned long)(pframe / PAGE_SIZE);
+	struct addrspace *as;
+	paddr_t paddr;
+	paddr_t pgvictim;
+	int index;
+	int result;
+	unsigned long total_npages;
+	unsigned long start;
+
+	as = proc_getas();
+
+	spinlock_acquire(&coremap->c_spinlock);
+	start = 0;
+	total_npages = coremap->c_npages;
+
+	for (unsigned long i=start; i<total_npages; i++) {
+		if (!coremap->c_entries[i].ce_allocated) {
+			KASSERT(!coremap->c_entries[i].ce_busy);
+			KASSERT(coremap->c_entries[i].ce_next == 0);
+			KASSERT(coremap->c_entries[i].ce_pgentry == NULL);
+			KASSERT(coremap->c_entries[i].ce_swapoffset ==
+			-1);
+
+			coremap->c_entries[i].ce_addrspace = as;
+			coremap->c_entries[i].ce_allocated = true;
+			coremap->c_entries[i].ce_foruser = true;
+			coremap->c_entries[i].ce_next = 0;
+			
+			paddr = (paddr_t)((i*PAGE_SIZE) >> 12);
+
+			*pg_entry &= ~PG_FRAME;
+			*pg_entry |= paddr; 
+			coremap->c_entries[i].ce_pgentry = pg_entry;
+			spinlock_release(&coremap->c_spinlock);
+			return 0;
+
+		}
+			
+	}
+
+	sw_getpage(&pgvictim);
+	index = (int)(pgvictim/PAGE_SIZE);
+	KASSERT(!coremap->c_entries[index].ce_busy);
+	coremap->c_entries[index].ce_busy = true;
 	
-	while (coremap->c_entries[index].next != 0) {
-		coremap->c_entries[index].next = 0;
-		KASSERT(coremap->c_entries[index].allocated == true);
-		coremap->c_entries[index].allocated = false;
+	result = sw_evictpage(pgvictim);
+	if (result) {
+		coremap->c_entries[index].ce_busy = false;
+		spinlock_release(&coremap->c_spinlock);
+		return result;
+	}
+
+	paddr = pgvictim >> 12;
+
+	*pg_entry &= ~PG_FRAME;
+	*pg_entry |= paddr; 
+	coremap->c_entries[index].ce_pgentry = pg_entry;
+	coremap->c_entries[index].ce_addrspace = as;
+	coremap->c_entries[index].ce_foruser = true;
+	coremap->c_entries[index].ce_next = 0;
+	coremap->c_entries[index].ce_pgentry = pg_entry;
+	coremap->c_entries[index].ce_swapoffset = -1;
+	coremap->c_entries[index].ce_busy = false;
+
+	spinlock_release(&coremap->c_spinlock);
+	return ENOMEM;
+}
+
+void
+coremap_freekpages(paddr_t paddr) {
+	int index;
+
+	spinlock_acquire(&coremap->c_spinlock);
+	index = (int)(paddr / PAGE_SIZE);
+	
+	while (coremap->c_entries[index].ce_next != 0) {
+		KASSERT(coremap->c_entries[index].ce_allocated);
+		KASSERT(!coremap->c_entries[index].ce_foruser);
+		KASSERT(!coremap->c_entries[index].ce_busy);
+		KASSERT(coremap->c_entries[index].ce_pgentry == NULL);
+		KASSERT(coremap->c_entries[index].ce_swapoffset == -1);
+			
+		coremap->c_entries[index].ce_allocated = false;
+		coremap->c_entries[index].ce_next = 0;
 		index++;
 	}
 
-	KASSERT(coremap->c_entries[index].allocated == true);
-	coremap->c_entries[index].allocated = false;
-	spinlock_release(&coremap->c_lock);
+	KASSERT(coremap->c_entries[index].ce_allocated);
+	KASSERT(!coremap->c_entries[index].ce_foruser);
+	KASSERT(!coremap->c_entries[index].ce_busy);
+	KASSERT(coremap->c_entries[index].ce_pgentry == NULL);
+	KASSERT(coremap->c_entries[index].ce_swapoffset == -1);
+
+	coremap->c_entries[index].ce_allocated = false;
+	spinlock_release(&coremap->c_spinlock);
+}
+
+void
+coremap_freepage(paddr_t pframe) {
+	int index;
+
+	spinlock_acquire(&coremap->c_spinlock);
+	index = (int)(pframe / PAGE_SIZE);
+
+	KASSERT(coremap->c_entries[index].ce_allocated);
+	KASSERT(coremap->c_entries[index].ce_foruser);
+	KASSERT(!coremap->c_entries[index].ce_busy);
+	KASSERT(coremap->c_entries[index].ce_next == 0);
+	KASSERT(coremap->c_entries[index].ce_pgentry != NULL);
+
+	coremap->c_entries[index].ce_allocated = false;
+	coremap->c_entries[index].ce_foruser = false;
+	coremap->c_entries[index].ce_busy = false;
+	coremap->c_entries[index].ce_next  = 0;
+	coremap->c_entries[index].ce_pgentry = NULL;
+
+	if (coremap->c_entries[index].ce_swapoffset >= 0) {
+		lock_acquire(kswap->sw_disklock);
+		bitmap_unmark(kswap->sw_diskoffset, (unsigned)index);
+		lock_release(kswap->sw_disklock);
+	}	
+
+	coremap->c_entries[index].ce_swapoffset = -1;
+
+	spinlock_release(&coremap->c_spinlock);
 }
