3167517856db60c5dc35b8219390ef920f4d9870
augustinew <augustinew@ece.ubc.ca>
2016-04-14 18:59:53 -0700

    Added comments

--- kern/include/coremap.h
+++ kern/include/coremap.h
@@ -33,23 +33,79 @@
 #include <types.h>
 #include <lib.h>
 
+/* Define the number of physical pages we reserve exclusively for the kernel.*/
+#define N_RESERVEDKPAGES 30
+
+/*
+ * coremap_entry struct
+ */
 struct coremap_entry {
 
+	/* If the physical page represented by the coremap entry is used by the
+	 * kernel, ce_addrspace is NULL.  Otherwise, if it is used in a page
+	 * table entry, ce_addrspace points to the address space containing that
+	 * page table. */
 	struct addrspace *ce_addrspace;
+
+	/* ce_allocated indicates if the physical page is free or not */
 	bool ce_allocated;
+
+	/* ce_foruser indicates if the physical page is used by the kernel or by
+	 * a user process. */
 	bool ce_foruser;
+
+	/* ce_busy is set if the physical page is in the process of being
+	 * evicted. */
 	bool ce_busy;
+
+	/* ce_next is only meaningful if the physical page is used by the
+	 * kernel.  The field ce_next is set to 1 if it is contiguous with
+	 * another kernel page. */
 	int ce_next;
+
+	/* ce_pgentry is only meaningful if the physical page is used by a user
+	 * process.  The field ce_pgentry points to the page table entry
+	 * referencing it. */
 	int *ce_pgentry;
+
+	/* ce_swapoffset is used if the physical page has been cleaned.  Wit our
+	 * current implementation, we assume all pages are dirty so this field
+	 * is unused. */
 	int ce_swapoffset;
 };
 
+/*
+ * coremap struct
+ */
 struct coremap {
+
+	/* c_entries is an array of coremap entries */
 	struct coremap_entry *c_entries;
+
+	/* c_spinlock is the spinlock protecting the coremap */
 	struct spinlock c_spinlock;
+
+	/* c_npages is the number of physical pages in the system */
 	unsigned long c_npages;
+
+	/* c_ready indicates if the kernel has finished initializing the
+	 * coremap.  Until the coremap is ready, kmalloc uses ram_stelamem to
+	 * allocate memory. */
 	bool c_ready;
+
+	/* c_kernelpbase is the first physical address which can be allocated
+	 * and freed by the kernel.  The physical addresses prior to c_kernelpbase
+	 * are for data structures created during bootup like the coremap.  It
+	 * is probably not a good idea to be freeing those... */
+	int c_kernelpbase;
+
+	/* c_userpbase is the first physical address which can be used by the
+	 * user process. The physical addresses prior to c_userpbase are used by
+	 * the kernel.*/
+	int c_userpbase;
 };
+
+/* Declarations of coremap functions */
 
 extern struct coremap *coremap;
 
@@ -64,7 +120,5 @@ paddr_t coremap_getkpages(unsigned long npages);
 int coremap_getpage(int *pg_entry, struct addrspace *as);
 
 void coremap_freekpages(paddr_t pframe);
-
-void coremap_freepage(paddr_t pframe); 
 
 #endif
