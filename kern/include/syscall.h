8547ab8639ed0b84c3f3cc963bc9b00e85e76626
augustinew <augustinew@ece.ubc.ca>
2016-04-14 19:01:33 -0700

    Added comments

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -57,19 +57,35 @@ __DEAD void enter_new_process(int argc, userptr_t argv, userptr_t env,
  */
 
 int sys_reboot(int code);
+
 int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
+
 int sys_open(const_userptr_t filename, int flags, mode_t mode, int *retval);
+
 int sys_close(int fd, int *retval);
+
 int sys_write(int fd, void *buf, size_t buflen, int *retval);
+
 int sys_read(int fd, void *buf, size_t buflen, int *retval);
+
 int sys_lseek(int fd, off_t pos, int whence, int* retval, int* retval_v1);
+
 int sys_dup2 (int oldfd, int newfd, int* retval);
+
 int sys___getcwd(void* buf, size_t buflen, int* retval);
+
 int sys_chdir(const_userptr_t pathname, int* retval);
+
 int sys_fork(struct trapframe *tf, pid_t* retval);
+
 int sys_execv(const_userptr_t program, userptr_t* args, int* retval);
+
 int sys_getpid(pid_t* retval);
+
 int sys_waitpid(pid_t pid, userptr_t status, int options, pid_t* retval);
+
 void sys__exit(int exitcode);
+
 int sys_sbrk(intptr_t amount, void* retval);
+
 #endif /* _SYSCALL_H_ */
